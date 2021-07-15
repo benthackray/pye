10052df813f2290cf67ba894fda6524182a979a8
Randy Chou <Randychou2020@gmail.com>
2021-07-15 12:50:50 -0400

    updated eslinter config

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,23 +6,7 @@ const withAuth = require('../utils/auth');
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
   try {
-    const dbGalleryData = await Gallery.findAll({
-      include: [
-        {
-          model: Painting,
-          attributes: ['filename', 'description'],
-        },
-      ],
-    });
-
-    const galleries = dbGalleryData.map((gallery) =>
-      gallery.get({ plain: true })
-    );
-
-    res.render('homepage', {
-      galleries,
-      loggedIn: req.session.loggedIn,
-    });
+    
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
