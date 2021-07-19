4efcc5df717041953567a3dd18a131598295e0c5
Randy Chou <Randychou2020@gmail.com>
2021-07-19 12:06:32 -0400

    readded logout button

--- public/js/create.js
+++ public/js/create.js
@@ -29,7 +29,6 @@ const createFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        alert('Successfully created Pie!');
         document.location.replace('/profile');
       } else {
         alert('Failed to create pie');
